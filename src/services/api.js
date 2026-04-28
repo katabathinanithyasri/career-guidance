b0844db073a7cf9d9184aac156185a89216f152d
katabathinanithyasri <katabathinanithyasri@gmail.com>
2026-04-28 15:12:11 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://career-guidance-backend-production-9c75.up.railway.app/api";
 
 export const apiRequest = async (endpoint, method = "GET", data = null) => {
   try {
